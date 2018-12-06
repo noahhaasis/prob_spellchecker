52d779490324375e3089a8bdc2dbc09614e12656
Noah Haasis <haasis_noah@yahoo.de>
2018-12-06 06:25:58 +0100

    Make the Usage text independant of the program name

--- spellchecker.c
+++ spellchecker.c
@@ -33,7 +33,7 @@ char *next_word_in_text(FILE *text);
 
 int main(int argc, char **argv) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: ./spellchecker dictionary text\n");
+        fprintf(stderr, "Usage: %s dictionary text\n", argv[0]);
         return 1;
     }
 
